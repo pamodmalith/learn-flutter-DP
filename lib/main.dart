ee585edd013ed492d92dbf3a6f9be77301a46d20
Pamod Malith <pamodmalith@gmail.com>
2025-03-17 15:57:19 +0530

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Easy Flutter App",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+          leading: Icon(Icons.menu, size: 30, color: Colors.white),
+          actions: [Icon(Icons.search, size: 30, color: Colors.white)],
+        ),
+        body: Center(
+          child: Text("Hello World!", style: TextStyle(fontSize: 18)),
+        ),
+      ),
     );
   }
 }
