d07d4d78f327673448e86ff47dc9fef6ffd87961
Pamod Malith <pamodmalith@gmail.com>
2025-03-18 11:05:07 +0530

    Container, Decoration, Padding, Margin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dp/container.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,24 +11,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: SafeArea(
-        child: Scaffold(
-          body: Container(
-            height: 200,
-            width: 200,
-            margin: EdgeInsets.all(30),
-            padding: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.blue,
-              border: Border.all(color: const Color(0xFFFF0000), width: 5),
-            ),
-            child: Center(child: Text("Flutter")),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(title: 'Flutter Demo', home: LCon());
   }
 }
