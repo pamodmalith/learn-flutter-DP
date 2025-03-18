116945f4fa8a554cbe990bf9f05abff00ff22c0f
Pamod Malith <pamodmalith@gmail.com>
2025-03-18 10:33:04 +0530

    Container, Decoration, Padding, Margin

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: SafeArea(
+        child: Scaffold(
+          body: Container(
+            height: 200,
+            width: 200,
+            margin: EdgeInsets.all(30),
+            padding: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.blue,
+              border: Border.all(color: const Color(0xFFFF0000), width: 5),
+            ),
+            child: Center(child: Text("Flutter")),
+          ),
+        ),
       ),
-      home: Scaffold(),
     );
   }
 }
